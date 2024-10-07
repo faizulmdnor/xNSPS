991787ddbf6da347fb928a04aefc46485591a0d4
Faizul Md Nor <71113657+faizulmdnor@users.noreply.github.com>
2024-10-07 15:48:07 +0800

    Create Exercise 01.py

---
+++ Exercise 01.py
@@ -0,0 +1,46 @@
+# Addition
+a = 10
+b = 5
+sum_result = a + b
+print("Sum:", sum_result)
+
+# Subtraction
+sub_result = a - b
+print("Difference:", sub_result)
+
+# Multiplication
+mul_result = a * b
+print("Product:", mul_result)
+
+# Division
+div_result = a / b
+print("Quotient:", div_result)
+
+# Modulus (remainder of the division)
+mod_result = a % b
+print("Remainder:", mod_result)
+
+'''
+Exercise: Even or Odd Checker
+Write a Python program that checks if a given number is even or odd.
+
+Prompt the user to enter a number.
+
+Check if the number is even or odd.
+
+Print the result.
+'''
+
+
+'''
+Exercise: Simple Calculator
+Create a Python program that takes two numbers and an operator from the user and performs the corresponding operation.
+
+Prompt the user to enter two numbers.
+
+Prompt the user to enter an operator (+, -, *, /).
+
+Perform the operation based on the operator.
+
+Print the result.
+'''
