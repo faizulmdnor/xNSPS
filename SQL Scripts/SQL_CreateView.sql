aad99e60c910b6e357523887dbb0c84a6f56c4d8
Faizul Md Nor <71113657+faizulmdnor@users.noreply.github.com>
2024-10-07 11:23:25 +0800

    Add CSV attribute settings and create department views
    Enhanced the .idea/csv-editor.xml file with attribute settings for password list CSV files. Updated the SQL script to include more descriptive comments and created specific views for various departments based on the vw_UserDetails view.

--- SQL Scripts/SQL_CreateView.sql
+++ SQL Scripts/SQL_CreateView.sql
@@ -1,29 +1,59 @@
--- Select user details with related gender, department, and username
+-- Query to select user details along with related gender, department, and username
 SELECT 
-    u.userid,                         -- User ID from 'Users' table
-    u.firstname,                      -- First name from 'Users' table
-    u.lastname,                       -- Last name from 'Users' table
-    u.date_of_birth,                  -- Date of birth from 'Users' table
-    g.gender,                         -- Gender from 'Genders' table (joined using gender_id)
-    d.dept,                           -- Department from 'Departments' table (joined using deptid)
-    m.username                        -- Username from 'Username' table (joined using userid)
+    u.userid,                         -- User's unique ID from 'Users' table
+    u.firstname,                      -- User's first name from 'Users' table
+    u.lastname,                       -- User's last name from 'Users' table
+    u.date_of_birth,                  -- User's date of birth from 'Users' table
+    g.gender,                         -- User's gender from 'Genders' table (joined via gender_id)
+    d.dept,                           -- User's department from 'Departments' table (joined via deptid)
+    m.username                        -- User's username from 'Username' table (joined via userid)
 FROM Users u
-LEFT JOIN Genders g ON g.gid = u.gender_id        -- Left join 'Genders' table to get gender
-LEFT JOIN Departments d ON d.deptid = u.deptid    -- Left join 'Departments' table to get department
-LEFT JOIN Username m ON m.userid = u.userid;      -- Left join 'Username' table to get username
+LEFT JOIN Genders g ON g.gid = u.gender_id        -- Joining 'Genders' table to get the gender field
+LEFT JOIN Departments d ON d.deptid = u.deptid    -- Joining 'Departments' table to get the department
+LEFT JOIN Username m ON m.userid = u.userid;      -- Joining 'Username' table to get the username
 
--- Alter the view vw_UserDetails to reflect the same user details with related gender, department, and username
+-- Select everything from the view vw_UserDetails, which contains user details with gender, department, and username
+SELECT * FROM vw_UserDetails;
+
+-- Altering the view vw_UserDetails to ensure it includes the same user details with related gender, department, and username
 ALTER VIEW vw_UserDetails
 AS
 SELECT 
-    u.userid,                         -- User ID from 'Users' table
-    u.firstname,                      -- First name from 'Users' table
-    u.lastname,                       -- Last name from 'Users' table
-    u.date_of_birth,                  -- Date of birth from 'Users' table
-    g.gender,                         -- Gender from 'Genders' table (joined using gender_id)
-    d.dept,                           -- Department from 'Departments' table (joined using deptid)
-    m.username                        -- Username from 'Username' table (joined using userid)
+    u.userid,                         -- User's unique ID from 'Users' table
+    u.firstname,                      -- User's first name from 'Users' table
+    u.lastname,                       -- User's last name from 'Users' table
+    u.date_of_birth,                  -- User's date of birth from 'Users' table
+    g.gender,                         -- User's gender from 'Genders' table (joined via gender_id)
+    d.dept,                           -- User's department from 'Departments' table (joined via deptid)
+    m.username                        -- User's username from 'Username' table (joined via userid)
 FROM Users u
-LEFT JOIN Genders g ON g.gid = u.gender_id        -- Left join 'Genders' table to get gender
-LEFT JOIN Departments d ON d.deptid = u.deptid    -- Left join 'Departments' table to get department
-LEFT JOIN Username m ON m.userid = u.userid;      -- Left join 'Username' table to get username
+LEFT JOIN Genders g ON g.gid = u.gender_id        -- Joining 'Genders' table to retrieve gender information
+LEFT JOIN Departments d ON d.deptid = u.deptid    -- Joining 'Departments' table to retrieve department information
+LEFT JOIN Username m ON m.userid = u.userid;      -- Joining 'Username' table to retrieve username
+
+-- Creating views for different departments by filtering the vw_UserDetails view
+
+-- Create a view for users in the 'Finance' department
+CREATE VIEW vw_Finance AS
+SELECT * FROM vw_UserDetails
+WHERE dept = 'Finance';
+
+-- Create a view for users in the 'Quality' department
+CREATE VIEW vw_Quality AS
+SELECT * FROM vw_UserDetails
+WHERE dept = 'Quality';
+
+-- Create a view for users in the 'Operations' department
+CREATE VIEW vw_Operations AS
+SELECT * FROM vw_UserDetails
+WHERE dept = 'Operations';
+
+-- Create a view for users in the 'Marketing' department
+CREATE VIEW vw_Marketing AS
+SELECT * FROM vw_UserDetails
+WHERE dept = 'Marketing';
+
+-- Create a view for users in the 'Information Technology' department
+CREATE VIEW vw_InformationTechnology AS
+SELECT * FROM vw_UserDetails
+WHERE dept = 'Information Technology';
