c79158c49179114a7899fc8a988f1aa645729f51
Faizul Md Nor <71113657+faizulmdnor@users.noreply.github.com>
2024-10-15 21:57:34 +0800

    Rename CSV file and add new death statistic dataset
    Renamed an existing CSV file for better clarity. Added a new comprehensive dataset detailing death statistics by state, sex, and age group for Malaysia from 2001 to 2021. This enhances the breadth and depth of the data available for analysis.

--- pandas 01.py
+++ pandas 01.py
@@ -1,15 +1,12 @@
 import pandas as pd
 import numpy as np
-from formatting import AnsiFontColors
 
 fpath = 'E:/X-NSPS/Python - Scripting/pythonProject/xNSPS/'
-fname = "UserDetails.csv"
+fname = "2011_2021_statistik_tangkapan_pesalah_rasuah.csv"
 filename = fpath+fname
 print(filename)
 
 # read csv data
 data = pd.read_csv(filename)
 print(data.head(10))
-print(data.columns)
-
-print
+print(data)
