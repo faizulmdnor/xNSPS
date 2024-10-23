100c4f5d0bf045228acf1470cff4149abfa3e891
Faizul Md Nor <71113657+faizulmdnor@users.noreply.github.com>
2024-10-23 17:13:37 +0800

    Update data transformation and insertion logic
    This commit refines the data processing pipeline by adding gender and department codes, and combines them with date of birth to create a unique identifier 'nom_kp'. Additionally, it integrates the insertion of processed user data into the 'User_noKP' table using a cursor for database operations.

--- Fezdbase2-get_data.py
+++ Fezdbase2-get_data.py
@@ -1,10 +1,13 @@
 import pyodbc
 import pandas as pd
 
+from insert_noKadPengenalan import cursor
+
 SERVER = 'FAIZULONXY\\SQLEXPRESS'
 DATABASE = 'Fezdbase2'
 
 conn = pyodbc.connect(f'DRIVER={{SQL Server}};SERVER={SERVER};DATABASE={DATABASE};Trusted_Connection=yes;')
+cursor = conn.cursor()
 
 tablename = 'vw_Users'
 
@@ -14,16 +17,55 @@ df_data = pd.read_sql(sql_query, conn)
 
 df_male = df_data[df_data['gender'] == 'Male']
 df_male.reset_index(drop=True, inplace=True)
-print(df_male)
 
 df_female = df_data[df_data['gender'] == 'Female']
 df_female.reset_index(drop=True, inplace=True)
-print(df_female)
 
-dept_code = {'Quality': 33,
-          'Finance': 44,
-          'Human Resources': 66,
-          'Information Technology': 77,
-          'Marketing': 88,
-          'Operations': 99}
+# Initialize the d_code column with default value
+df_data['d_code'] = 11
 
+# Define a mapping for department codes
+dept_codes = {
+    'Quality': 33,
+    'Finance': 44,
+    'Human Resources': 66,
+    'Information Technology': 77,
+    'Marketing': 88,
+    'Operations': 99
+}
+
+# Assign department codes based on the mapping
+for dept, code in dept_codes.items():
+    df_data.loc[df_data['dept'] == dept, 'd_code'] = code
+
+# Convert d_code to integer type
+df_data['d_code'] = df_data['d_code'].astype(int)
+
+# Initialize gender count variables
+m_count = 3030
+f_count = 4039
+
+# Initialize the g_code column
+df_data['g_code'] = 0
+
+# Assign g_code based on gender
+for index, row in df_data.iterrows():
+    if row['gender'] == 'Male':
+        df_data.at[index, 'g_code'] = m_count
+        m_count += 2
+    elif row['gender'] == 'Female':
+        df_data.at[index, 'g_code'] = f_count
+        f_count += 2
+
+df_data['date_of_birth'] = pd.to_datetime(df_data['date_of_birth'])
+df_data['b_code'] = df_data['date_of_birth'].dt.strftime('%y%m%d')
+
+# nom_kp is stand for 'Nombor Kad Pengenalan"
+df_data['nom_kp'] = df_data['b_code'].astype(str) + '-' + df_data['d_code'].astype(str) + "-" + df_data[
+    'g_code'].astype(str)
+df_data2 = df_data[['userid', 'firstname', 'lastname', 'date_of_birth', 'gender', 'dept', 'nom_kp']]
+
+for i, r in df_data2.iterrows():
+    cursor.execute(
+        f'INSERT INTO User_noKP userid, firstname, lastname, date_of_birth, gender, dept, nom_kp VALUES (?, ?. ?, ?, ?, ?, ?, ?)',
+        r['userid'], r['firstname'], r['lastname'], r['date_of_birth'], r['gender'], r['dept'], r['nom_kp'])
