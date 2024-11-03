6c8b82da4a85ed665f09d39f978cb46c9c927b71
Faizul Md Nor <71113657+faizulmdnor@users.noreply.github.com>
2024-11-04 03:10:06 +0800

    update/modify date of birth

---
+++ GreenVolt/GreenVolt_UPDATE_Date_of_Birth.py
@@ -0,0 +1,53 @@
+import pyodbc
+import pandas as pd
+from datetime import datetime
+
+SERVER = 'FAIZULONXY\\SQLEXPRESS'
+DATABASE = 'GreenVolt'
+conn = pyodbc.connect(f'DRIVER={{SQL Server}};SERVER={SERVER};DATABASE={DATABASE};Trusted_Connection=yes;')
+cursor = conn.cursor()
+
+# Fetch data from SQL
+sql_query = """
+    SELECT * FROM Employees 
+    WHERE Date_of_Birth > DATEADD(YEAR, 1, GETDATE())
+"""
+data = pd.read_sql(sql_query, conn)
+
+# Update dates in the DataFrame
+new_dates = []
+for date in data['Date_of_Birth']:
+    # Convert to datetime format
+    if isinstance(date, str):
+        date_obj = datetime.strptime(date, '%Y-%m-%d')
+    else:
+        date_obj = date
+
+    # Replace year with 1900s
+    new_date_obj = date_obj.replace(year=int("19" + str(date_obj.year)[2:4]))
+    new_dates.append(new_date_obj.strftime('%Y-%m-%d'))
+
+# Assign the modified dates back to the DataFrame
+data['Date_of_Birth'] = new_dates
+
+# Update each row in the database
+for index, row in data.iterrows():
+    new_date = row['Date_of_Birth']
+    empid = row['emp_id']
+
+    sql_update = f"""
+        UPDATE Employees
+        SET Date_of_Birth = '{new_date}'
+        WHERE emp_id = {empid}
+    """
+
+    # Execute the query with parameters
+    try:
+        cursor.execute(sql_update)
+        conn.commit()  # Commit the update after each execution
+    except Exception as err:
+        print(f"Error updating emp_id {empid}: {err}")
+
+# Close the connection
+cursor.close()
+conn.close()
