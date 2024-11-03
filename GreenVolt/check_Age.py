0502fb2d3b0582afa3ad9effc6b940dcfe72c2c8
Faizul Md Nor <71113657+faizulmdnor@users.noreply.github.com>
2024-11-04 03:19:59 +0800

    results from update date of birth

--- GreenVolt/check_Age.py
+++ GreenVolt/check_Age.py
@@ -1,7 +1,6 @@
 import pandas as pd
 import pyodbc
 
-
 SERVER = 'FAIZULONXY\\SQLEXPRESS'
 DATABASE = 'GreenVolt'
 conn = pyodbc.connect(f'DRIVER={{SQL Server}};SERVER={SERVER};DATABASE={DATABASE};Trusted_Connection=yes;')
@@ -26,4 +25,28 @@ data['Hired_period'] = (today - data['Date_Hired']).dt.days // 365.25
 data['Age'].fillna(0, inplace=True)
 data['Hired_period'].fillna(0, inplace=True)
 
-data.to_csv('E:/X-NSPS/Python - Scripting/pythonProject/xNSPS/GreenVolt/employees_info.csv')
+modify_status = data[data['Hired_period'] < 0]
+
+try:
+
+    for i, r in modify_status.iterrows():
+        empid = r['emp_id']
+        sql_update_status = f"""
+            UPDATE Employees
+            SET status_id = 0
+            WHERE emp_id = {empid}
+        """
+        cursor.execute(sql_update_status)
+        conn.commit()
+
+except Exception as e:
+    print(e)
+    conn.rollback()
+
+finally:
+    if cursor is not None:
+        cursor.close()
+    if conn is not None:
+        conn.close()
+
+data.to_csv('E:/X-NSPS/Python - Scripting/pythonProject/xNSPS/GreenVolt/employees_info.csv', index=False)
