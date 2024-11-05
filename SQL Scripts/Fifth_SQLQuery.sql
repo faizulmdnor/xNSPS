28d5b4437155f8e6fe887f5f78b378043f6ea2ee
Faizul Md Nor <71113657+faizulmdnor@users.noreply.github.com>
2024-11-05 16:36:42 +0800

    add query to get top 5 regions

--- SQL Scripts/Fifth_SQLQuery.sql
+++ SQL Scripts/Fifth_SQLQuery.sql
@@ -18,6 +18,14 @@ OFFSET 4 ROWS FETCH NEXT 1 ROW ONLY;
 
 SELECT * FROM Region2
 
+
+
+SELECT region_name, SUM(revenue) AS total_revenue
+FROM region2
+WHERE date > DATEADD(day, -28, GETDATE())
+GROUP BY region_name
+ORDER BY total_revenue DESC
+
 SELECT region_name, SUM(revenue) AS total_revenue
 FROM region2
 WHERE date > DATEADD(day, -28, GETDATE())
